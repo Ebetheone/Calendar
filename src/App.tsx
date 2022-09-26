2c5d33be3445c148864b754c6f0b3f9a1f371bf8
Elbeg Darkhanbaatar <elbeg.db@yahoo.com>
2022-09-26 11:21:50 +0800

    feat: done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { format } from "date-fns";
+import { useState } from "react";
+import Calendar from "./components/Calendar";
+import Button from "./components/Button";
 
-function App() {
-  return <div className="w-full h-screen"></div>;
-}
+const App = () => {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [show, setShow] = useState<boolean>(true);
+  const handleSetToday = () => {
+    setCurrentDate(new Date());
+  };
+
+  return (
+    <div className="">
+      {show ? (
+        <div className="h-screen w-full flex justify-center items-center">
+          <button
+            className="flex bg-black text-white justify-center items-center border-2 border-solid border-[rgb(0,0,0)] rounded-[12px] px-4 py-2 font-bold font-serif text-[20px]"
+            onClick={() => setShow(false)}
+          >
+            Calendar
+          </button>
+        </div>
+      ) : (
+        <div className="h-screen w-full flex justify-center items-center">
+          <div className="flex flex-col items-center gap-8">
+            <div className="flex flex-col items-center gap-4">
+              <p>
+                <strong>Picked Date: </strong>
+                {format(currentDate, "dd LLLL yyyy")}
+              </p>
+              <Button onClick={handleSetToday}>Today</Button>
+            </div>
+            <Calendar value={currentDate} onChange={setCurrentDate} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren } from "react";
+import clsx from "clsx";
+
+interface Props extends PropsWithChildren {
+  onClick?: () => void;
+  className?: string;
+}
+const Button: React.FC<Props> = ({ onClick, className, children }) => (
+  <button
+    onClick={onClick}
+    className={clsx(
+      "text-white bg-black text-sm px-4 py-1.5 rounded",
+      className
+    )}
+  >
+    {children}
+  </button>
+);
+
+export default Button;
